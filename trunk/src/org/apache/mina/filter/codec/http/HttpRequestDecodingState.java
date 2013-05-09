bda85d8474abcfc979d038eb268b7c7ce92264ba
Nike.Lius@gmail.com <Nike.Lius@gmail.com@ca9d2e37-6aae-3169-032f-146d44a03d81>
2013-05-09 08:02:44 +0000

    bs3-svn-296 //20090720 （贺林）处理close时候IndexOutOfBoundsException错误。（HttpRequestDecodingState.java:88）

--- trunk/src/org/apache/mina/filter/codec/http/HttpRequestDecodingState.java
+++ trunk/src/org/apache/mina/filter/codec/http/HttpRequestDecodingState.java
@@ -83,15 +83,27 @@ abstract class HttpRequestDecodingState extends DecodingStateMachine {
 
     private final DecodingState READ_REQUEST_LINE = new HttpRequestLineDecodingState() {
         @Override
-        protected DecodingState finishDecode(List<Object> childProducts,
-                ProtocolDecoderOutput out) throws Exception {
-            URI requestUri = (URI) childProducts.get(1);
-            request.setMethod((HttpMethod) childProducts.get(0));
-            request.setRequestUri(requestUri);
-            request.setProtocolVersion((HttpVersion) childProducts.get(2));
-            request.setParameters(requestUri.getRawQuery());
-            return READ_HEADERS;
+        protected DecodingState finishDecode(List<Object> childProducts, ProtocolDecoderOutput out) throws Exception {
+        	if (childProducts.size()<=1) {// 20090720 DONE ´¦ÀícloseÊ±ºòIndexOutOfBoundsException´íÎó¡££¨HttpRequestDecodingState.java:88£©
+	            URI requestUri = (URI) childProducts.get(1);//java.lang.IndexOutOfBoundsException: Index: 1, Size: 0
+	            request.setMethod((HttpMethod) childProducts.get(0));
+	            request.setRequestUri(requestUri);
+	            request.setProtocolVersion((HttpVersion) childProducts.get(2));
+	            request.setParameters(requestUri.getRawQuery());
+	            return READ_HEADERS;
+        	}else {
+        		return null;
+        	}
         }
+//      0720.152311.477 [pool-1-thread-2 ] DEBUG L:166 DecodingStateMachine - Ignoring the exception caused by a closed session.
+//      java.lang.IndexOutOfBoundsException: Index: 1, Size: 0
+//      	at java.util.ArrayList.RangeCheck(ArrayList.java:546)
+//      	at java.util.ArrayList.get(ArrayList.java:321)
+//      	at org.apache.mina.filter.codec.http.db.finishDecode(HttpRequestDecodingState.java:88)
+//      	at org.apache.mina.filter.codec.statemachine.DecodingStateMachine.finishDecode(DecodingStateMachine.java:170)
+//      	at org.apache.mina.filter.codec.statemachine.DecodingStateMachine.finishDecode(DecodingStateMachine.java:153)
+//      	at org.apache.mina.filter.codec.statemachine.DecodingStateProtocolDecoder.finishDecode(DecodingStateProtocolDecoder.java:100)
+//      	at org.apache.mina.filter.codec.ProtocolCodecFilter.sessionClosed(ProtocolCodecFilter.java:347)
     };
 
     private final DecodingState READ_HEADERS = new HttpHeaderDecodingState() {
